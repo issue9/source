25bab393c1a3332852fd0980529ec10c696abe72
caixw <xnotepad@gmail.com>
2023-06-28 18:11:17 +0800

    test: 修正非 windows 下测试错误

--- mod_test.go
+++ mod_test.go
@@ -3,6 +3,7 @@
 package source
 
 import (
+	"runtime"
 	"testing"
 
 	"github.com/issue9/assert/v3"
@@ -18,6 +19,10 @@ func TestModFile(t *testing.T) {
 	a.NotError(err).NotNil(f).Equal(f.Module.Mod.Path, "github.com/issue9/source")
 
 	// NOTE: 可能不存在 c:\windows\system32 或是 c:\windows\system32 下正好存在一个 go.mod
-	f, err = ModFile("c:\\windows\\system32")
+	dir := "/windows/system32"
+	if runtime.GOOS == "windows" {
+		dir = "c:\\windows\\system32"
+	}
+	f, err = ModFile(dir)
 	a.Error(err).Nil(f)
 }
